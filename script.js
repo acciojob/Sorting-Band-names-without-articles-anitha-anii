f39c4a0250fe51cd623019479ea7aa66feb7046b
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-16 16:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+touristSpots.sort((a, b) => {
+    const articles = /^(The|A|An)\s+/i; 
 
+    const spotA = a.replace(articles, '');
+    const spotB = b.replace(articles, '');
+  
+    return spotA.localeCompare(spotB);
+  });
+  
+  console.log(touristSpots);
